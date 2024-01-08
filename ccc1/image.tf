499e78c8ad0e0b21a4a8a663df02790b2bf6958f
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2024-01-08 15:40:34 +0100

    ccc1: SWIM from mirror

--- ccc1/image.tf
+++ ccc1/image.tf
@@ -1,3 +1,3 @@
 resource "catalystcenter_image_from_url" "example" {
-  source_url = "https://example.com/software.bin"
+  source_url = "http://ftpmirror.uk/pub/Software/Cisco/3850/cat3k_caa-universalk9.16.12.07.SPA.bin"
 }
