5db020af387a91b9fc4e5899d6c66210dfbc58ad
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2024-02-01 23:19:11 +0100

    TODO experiment lab/image

--- lab/image/main.tf
+++ lab/image/main.tf
@@ -3,20 +3,29 @@ locals {
   source_path  = try(local.source_paths[0], "")
 }
 
-resource "catalystcenter_image" "example" {
+# resource "catalystcenter_image" "example" {
 
-  source_path = local.source_path
-  name        = basename(local.source_path)
+#   source_path = local.source_path
+#   name        = basename(local.source_path)
 
-  lifecycle {
-    precondition {
-      condition     = length(fileset(path.module, "*.bin")) > 0
-      error_message = "Software image file not found! Please put it here: ${abspath(path.module)}/*.bin"
-    }
-  }
+#   lifecycle {
+#     precondition {
+#       condition     = length(fileset(path.module, "*.bin")) > 0
+#       error_message = "Software image file not found! Please put it here: ${abspath(path.module)}/*.bin"
+#     }
+#   }
 
-}
+# }
 
-output "catalystcenter_image" {
-  value = catalystcenter_image.example
+# output "catalystcenter_image" {
+#   value = catalystcenter_image.example
+# }
+
+resource "catalystcenter_image" "test" {
+  third_party_application_type = "UNKNOWN"
+  family                       = "TF"
+  source_path                  = "/tmp/TF.1.2.3-comp_matrix.zip"
+  name                         = basename("/tmp/TF.1.2.3-comp_matrix.zip")
+  third_party_vendor           = "CISCO"
+  is_third_party               = true
 }
